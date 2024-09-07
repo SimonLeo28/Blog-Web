d945ef975a1307e2c8f55626f7f7ddb5eefb1a65
SimonLeo28 <simonleoalexanderpcmct2023@gmail.com>
2024-09-07 20:20:07 +0530

    added the database settings.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "Blog_Database",
+    password: "$Simon12345",
+    port: 5432,
+  });
+  db.connect();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended : true }));
@@ -19,14 +29,25 @@ app.get("/home" , (req,res) => {
     res.render("index.ejs");
 });
 
-app.get("/blogs" , (req,res) => {
-    res.render("blogs.ejs");
+app.get("/blogs" , async (req,res) => {
+    const result = await db.query("SELECT * FROM blogs ORDER BY id ASC");
+        const blogs = result.rows;
+        res.render("blogs.ejs" , {blogs: blogs});
 });
 
-app.post("/submit" , (req,res) => {
+app.post("/submit" , async (req,res) => {
     const titl = req.body["title"];
     const cont = req.body["content"];
-    res.render("blogs.ejs" , {title : titl , content : cont});
+    try {
+        await db.query("INSERT INTO blogs (title , content) VALUES ($1,$2)",[titl,cont]);
+        const result = await db.query("SELECT * FROM blogs");
+        const blogs = result.rows;
+        res.render("blogs.ejs" , { blogs : blogs }); 
+        
+        
+    } catch(err) {
+        console.log(err);        
+    }
 });
 
 app.listen(port, () => {
